f3f9449827d7ae462295bbdf5b9b1e90dd1c3b89
Fahim Morshed Nion <114305756+chonion-04022022@users.noreply.github.com>
2024-01-02 16:13:46 +0600

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ document.querySelectorAll("img")[0].setAttribute("src",imgExt);
 document.querySelectorAll("img")[1].setAttribute("src", imgExt2);
 
 if (randomNum>randomNum2){
-    document.querySelector("h1").innerHTML = "🎉🏆 Player1 Wins."
+    document.querySelector("h1").innerHTML = "🎉🏆 Player1 Wins.";
 }
 else if (randomNum===randomNum2){
-    document.querySelector("h1").innerHTML ="😪 Sorry Nobody gets to go home with the Trophy"
+    document.querySelector("h1").innerHTML ="😪 Sorry Nobody gets to go home with the Trophy";
 }
 else{
-    document.querySelector("h1").innerHTML="🎉🏆 Player2 Wins."
+    document.querySelector("h1").innerHTML="🎉🏆 Player2 Wins.";
 }
