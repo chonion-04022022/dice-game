f133a8b41349dc782e8557f2bfdb8bbab6c50661
Fahim Morshed Nion <114305756+chonion-04022022@users.noreply.github.com>
2024-01-02 16:10:29 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+var randomNum= Math.floor(Math.random()*6)+1;
+var imgExt= "images/dice"+randomNum+".png";
+var randomNum2= Math.floor(Math.random()*6)+1;
+var imgExt2= "images/dice"+randomNum2+".png";
+document.querySelectorAll("img")[0].setAttribute("src",imgExt);
+document.querySelectorAll("img")[1].setAttribute("src", imgExt2);
+
+if (randomNum>randomNum2){
+    document.querySelector("h1").innerHTML = "🎉🏆 Player1 Wins."
+}
+else if (randomNum===randomNum2){
+    document.querySelector("h1").innerHTML ="😪 Sorry Nobody gets to go home with the Trophy"
+}
+else{
+    document.querySelector("h1").innerHTML="🎉🏆 Player2 Wins."
+}
